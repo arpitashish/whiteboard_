eb79ab2cc40dd1bef511d414c201312d1d12f8ac
Arpit Ashish Raj <arpitashishraj@gmail.com>
2026-09-03 16:08:09 +0530

    Send JWT through Socket.IO auth

--- whiteboard_az-main/whiteboard-tutorial/src/utils/socket.js
+++ whiteboard_az-main/whiteboard-tutorial/src/utils/socket.js
@@ -3,7 +3,9 @@ import { io } from "socket.io-client";
 const token = localStorage.getItem("whiteboard_user_token");
 
 const socket = io("https://whiteboard-d37k.onrender.com", {
-  extraHeaders: token ? { Authorization: `Bearer ${token}` } : {},
+  auth: {
+    token: token,
+  },
 });
 
 export default socket;
